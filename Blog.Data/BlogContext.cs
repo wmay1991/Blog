6b8d6454ff757872a56d5832b1df480d9c44bcaa
Whitney May <maywhitney@rocketmail.com>
2015-05-07 11:48:05 -0400

    Data and Domain projects

---
+++ Blog.Data/BlogContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Blog.Domain;
+
+namespace Blog.Data
+{
+    public class BlogContext : DbContext
+    {
+        public BlogContext()
+            : base("BlogContext")
+        {
+            
+        }
+        public DbSet<Blogs> Blogs { get; set; }
+    }
+}
---
+++ Blog.Domain/Blog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq.Mapping;
+using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Domain
+{
+   [Table(Name = "Blogs")]
+    public class Blogs
+    {
+        public Blogs()
+        {
+
+        }
+
+        [Key] // Primary Key
+        public int blogId { get; set; }
+
+        public string blogName {get;set;}
+        public string tease {get;set;}
+        public string body {get; set;}
+
+    }
+}
